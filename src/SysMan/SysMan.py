aec6bb49fc244ad31b9ebe811b0c61c93f6b2ffe
Entshceidbarkeit <thomexbfsu@gmail.com>
2025-01-09 22:07:21 +0100

    Fitcut optimization implemented

--- src/SysMan/SysMan.py
+++ src/SysMan/SysMan.py
@@ -1,11 +1,14 @@
 import copy
 import math
 import random
+
+from memory_profiler import profile
 from qiskit import QuantumCircuit,transpile
 from qiskit.converters import circuit_to_dag
 from qiskit.dagcircuit import DAGCircuit, DAGOpNode
 import networkx as nx
-
+import matplotlib.pyplot as plt
+import gc
 from src.utils import Qbits
 
 
@@ -96,21 +99,28 @@ class SystemManager :
 
         return closest_worker
 
-    def circuit_scheduling(self,W:dict,P:dict):
+    def circuit_scheduling(self,graph:nx.Graph,W:dict,P:dict,community:dict):
 
         """
         view of A:
 
         {worker_id : {"partitions" : [ids of partition] , 
                       "capacities" : qubits capacity in Integer}}
+
+        P :
+        {partition_id : [communities]}
         """
         A = {}
         for worker in W.keys(): # init
             A[worker] = {"partitions": [], "capacities": W[worker]}
         
-        for p_i,p_cap in P.items():
+        for p_i,communities in P.items():
+            nodes = self.unfold_partition(communities, community)
+            subg = graph.subgraph(nodes)
+            p_cap = Qbits(subg)
             w_id = self.find_closest_worker(p_cap,W)
             A[w_id]["partitions"] += [p_i]
+
         
         A = dict(sorted(A.items(), key=lambda item: item[1]["capacities"], reverse=True)) # from python 3.7 dict can be accessed by sorted result
         for w_id_i in A.keys():
@@ -142,7 +152,7 @@ class SystemManager :
             partition_nodes += community[id]
         return partition_nodes
 
-    def obj(self, graph:nx.Graph,A:dict, input_qbits, community:dict):
+    def obj(self, graph:nx.Graph,A:dict, input_qbits, community:dict , partition:dict):
         nc = 0
         ru = 0
         sum_qbits = 0
@@ -154,8 +164,19 @@ class SystemManager :
         nc = sum_qbits - input_qbits
         diff = 0
         for worker_scheduled in A.values():
-            pass
-    
+            for scheduled_partition in worker_scheduled["partitions"]:
+                nodes = self.unfold_partition_for_scheduler(scheduled_partition,partition,community)
+                subg = graph.subgraph(nodes)
+                diff += Qbits(subg) - input_qbits
+        return nc, diff
+
+    def unfold_partition_for_scheduler(self, partition_id, partition:dict, community: dict):
+        coms = partition[partition_id]
+        nodes = []
+        for com in coms:
+            nodes += community[com]
+        return nodes
+
     def merge_communities_in_graph(self, graph:nx.Graph, communities):
         """
         merge the communities as one node based on given graph and return the merged graph.
@@ -171,17 +192,20 @@ class SystemManager :
 
         for community_id, nodes in communities.items():
             merged_graph.add_node(community_id)
-            
+
+        for community_id, nodes in communities.items():
             for node in nodes:
                 for neighbor, edge_data in graph[node].items():
                     if neighbor in nodes:
                         continue
-                    if neighbor not in communities:
-                        if merged_graph.has_edge(community_id, neighbor):
-                            merged_graph[community_id][neighbor]['weight'] += edge_data.get('weight', 1)
-                        else:
-                            merged_graph.add_edge(community_id, neighbor, weight=edge_data.get('weight', 1))
-
+                    for community_id_2, nodes_2 in communities.items():
+                        if neighbor in nodes_2:
+                            if merged_graph.has_edge(community_id, neighbor):
+                                merged_graph[community_id][community_id_2]['weight'] += edge_data.get('weight', 1)
+                            else:
+                                merged_graph.add_edge(community_id, community_id_2, weight=edge_data.get('weight', 1))
+        nx.draw(merged_graph,with_labels=True)
+        plt.show()
         return merged_graph
 
     def fitCut_Optimization(self, graph:nx.Graph, community: dict, W:dict, W_max_qbits, input_qbits):
@@ -190,35 +214,70 @@ class SystemManager :
         P = {}
         neighbour = {} # neighbour is a dictionary that stores the id of community and its neighbour partition
         for id in community.keys():
-            P[id] = id
+            P[id] = [id]
             neighbour[id] = list(merged_graph.neighbors(id)) # at the beginning the neighbout of each community is just other communities
         
         improvement  = True
-
+        num_of_partitions = len(community.keys())
         while improvement:
             improvement = False
-            for p_id,p_value in P.items():
+            for i in range(num_of_partitions): # use index instead, avoiding changing loop condition
+                p_id, p_value = list(P.items())[i]
                 bo_1 = float("inf")
                 bo_2 = float("inf")
                 for community_id in p_value: # for each community in a partition
-                    for neighbour_partitions in neighbour[community_id]: # for each neighbouring partition
-                        P_updated = copy.deepcopy(P)
-                        P_updated[neighbour_partitions].append(community_id)
+                    for neighbour_partitions in neighbour[community_id]:
+                        gc.collect()
+                        # print("all neighbour: ",neighbour)# for each neighbouring partition
+                        # print("neighbour: " ,neighbour[community_id])
+                        #print("neighbour_partitions: " ,neighbour_partitions)
+                        print("P: ", P)
+                        # print("community: ",community)
+                        # print("community_id: ",community_id)
+                        #print("copy")
+                        # P_updated = copy.deepcopy(P)
+                        # P_updated[p_id].remove(community_id)
+                        # P_updated[neighbour_partitions].append(community_id) # try assign a community from neighbour partition to current partition
+                        P_updated = P.copy()
+                        P_updated[p_id] = P[p_id][:]
                         P_updated[p_id].remove(community_id)
-                        neighbour_updated = self.neighbourUpdate(neighbour) #TODO
-                        if self.maxPartitionQubits() < W_max_qbits: #TODO
-                            A = self.circuit_scheduling(W,P_updated)
-                            nc,ru = self.obj(graph,A,input_qbits,community)
+                        P_updated[neighbour_partitions] = P[neighbour_partitions][:]
+                        P_updated[neighbour_partitions].append(community_id)
+                        #print(P_updated)
+                        neighbour = self.neighbourUpdate(neighbour_partitions,community_id,neighbour)
+                        if self.maxPartitionQubits(graph,P_updated,community) < W_max_qbits:
+                            A = self.circuit_scheduling(graph,W,P_updated,community)
+                            nc,ru = self.obj(graph,A,input_qbits,community,P_updated)
                             if nc< bo_1:
-                                P = P_updated # think of a logic that wont interupte the loop
+                                P = P_updated
                                 bo_1 = nc
                                 bo_2 = ru
                             elif nc == bo_1 and ru < bo_2:
                                 P = P_updated
                                 bo_2 = ru
                                 improvement = True
+
         A = self.circuit_scheduling(W,P)
+        print("finished optimization")
         return P,A
+
+    def neighbourUpdate(self,p_id,community_id,neighbour):
+        neighbour[p_id] += neighbour[community_id]
+        neighbour[community_id].remove(p_id)
+        return neighbour
+
+    def maxPartitionQubits(self,graph:nx.Graph,partition,communities):
+        qbits = []
+        for parts in partition.values():
+            node_ids = []
+            for partition_id in parts:
+                node_ids+= communities[partition_id]
+            subg = graph.subgraph(node_ids)
+            qbits.append(Qbits(subg))
+
+        return max(qbits)
+
+
 
             
 
